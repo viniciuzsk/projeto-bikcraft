8a18ce7e2f6cd7791d02c8984d1193b031e8264d
Vinicius Willian <viniciuz.dev@outlook.com>
2023-10-20 20:33:56 -0400

    galeria imagens

--- js/script.js
+++ js/script.js
@@ -37,7 +37,6 @@ function ativarPergunta(event) {
   resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains("ativa");
   pergunta.setAttribute("aria-expanded", ativa);
-
 }
 
 function eventosPerguntas(pergunta) {
@@ -45,3 +44,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
